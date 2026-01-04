d3c68fbea89c9f3c02121e1dc0ee546d80264853
Hylke Reinders <hylkereinders@gmail.com>
2026-01-04 15:31:23 +0100

    Updated why this file is not tested.

--- lib/src/adapters/http_adapter.dart
+++ lib/src/adapters/http_adapter.dart
@@ -7,6 +7,19 @@ import '../models/request.dart';
 import '../models/response.dart';
 import 'adapter.dart';
 
+/// HTTP transport adapter based on `package:http`.
+///
+/// This adapter is intentionally thin and contains no business logic.
+/// It delegates all network behavior to `package:http`.
+///
+/// As a result, this adapter is not exhaustively unit tested:
+/// - request semantics are tested at the `CoffeeHttp` level
+/// - network correctness is the responsibility of `package:http`
+///
+/// The adapter exists to:
+/// - translate `CoffeeRequest` into concrete HTTP calls
+/// - keep transport concerns isolated
+/// - allow alternative adapters in the future
 class HttpPackageAdapter extends CoffeeTransportAdapter {
   final CoffeeHttpConfig _config;
 
@@ -18,51 +31,26 @@ class HttpPackageAdapter extends CoffeeTransportAdapter {
   }
 
   @override
-  Future<CoffeeRawResponse> send(
-    CoffeeRequest request, {
-    required Map<String, String> headers,
-  }) async {
+  Future<CoffeeRawResponse> send(CoffeeRequest request, {required Map<String, String> headers}) async {
     Uri uri = _config.baseUrl.build(request.path, query: request.query);
 
     http.Response res;
 
     switch (request.method) {
       case CoffeeHttpMethod.get:
-        res = await http
-            .get(uri, headers: headers)
-            .timeout(_config.timeouts.receiveTimeout);
+        res = await http.get(uri, headers: headers).timeout(_config.timeouts.receiveTimeout);
 
       case CoffeeHttpMethod.post:
-        res = await http
-            .post(
-              uri,
-              headers: headers,
-              body: _encodeJsonBody(request.jsonBody),
-            )
-            .timeout(_config.timeouts.receiveTimeout);
+        res = await http.post(uri, headers: headers, body: _encodeJsonBody(request.jsonBody)).timeout(_config.timeouts.receiveTimeout);
 
       case CoffeeHttpMethod.put:
-        res = await http
-            .put(uri, headers: headers, body: _encodeJsonBody(request.jsonBody))
-            .timeout(_config.timeouts.receiveTimeout);
+        res = await http.put(uri, headers: headers, body: _encodeJsonBody(request.jsonBody)).timeout(_config.timeouts.receiveTimeout);
 
       case CoffeeHttpMethod.patch:
-        res = await http
-            .patch(
-              uri,
-              headers: headers,
-              body: _encodeJsonBody(request.jsonBody),
-            )
-            .timeout(_config.timeouts.receiveTimeout);
+        res = await http.patch(uri, headers: headers, body: _encodeJsonBody(request.jsonBody)).timeout(_config.timeouts.receiveTimeout);
 
       case CoffeeHttpMethod.delete:
-        res = await http
-            .delete(
-              uri,
-              headers: headers,
-              body: _encodeJsonBody(request.jsonBody),
-            )
-            .timeout(_config.timeouts.receiveTimeout);
+        res = await http.delete(uri, headers: headers, body: _encodeJsonBody(request.jsonBody)).timeout(_config.timeouts.receiveTimeout);
     }
 
     return CoffeeRawResponse(
